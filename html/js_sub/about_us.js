14d333fd48cfec74f2167a428aae86757aae82e3
davinch0821 <58626904+davinch0821@users.noreply.github.com>
2022-07-19 16:08:19 +0900

    1608

--- html/js_sub/about_us.js
+++ html/js_sub/about_us.js
@@ -3,8 +3,9 @@ document.body.oncontextmenu = "return false;"
 
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
+/* if you scroll, the header-bar will fead-out-move at 350pt just one time.*/
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset - menu.offsetTop >= 200) {
+    if (window.pageYOffset - menu.offsetTop >= 350) {
         //stickyクラスをnavbarに付与する。
         navbar.classList.add("sticky");
     } else {
